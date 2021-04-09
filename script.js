35d91c6de46b8f6bb86e99ff9616e9bf15f06d7a
Kabir Lakade <42903199+letscodeboys@users.noreply.github.com>
2021-04-10 01:31:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+
+    
+}
+var principal = "principal";
+var rate = "rate";
+var years = "years";
+var interest = "principal * years * rate / 100";
+var amount = "principal+interest";
+<button onclick="compute()">If you deposit ,
+at an interest rate of .
+You will receive an amount of ,
+in the year 2025</button>
+
+        
